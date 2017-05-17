177316551029a43470cc3cfd7979a4a98df28678
hanifsudira <sudirahanif@gmail.com>
2017-05-17 12:56:00 +0700

    halo

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class welcome extends CI_Controller {
+class Welcome extends CI_Controller {
 
 	public function index(){
         $data['page'] = 'War Room Dashboard';
