0e0022c7f692d73d24f6ed77ed45c346bf2afae3
hanifsudira <sudirahanif@gmail.com>
2017-05-18 19:16:45 +0700

    1

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -5,7 +5,6 @@ class Welcome extends CI_Controller {
     public function __construct(){
         parent::__construct();
         $this->load->database();
-        $this->load->model('dashboard');
     }
 
 	public function index(){
